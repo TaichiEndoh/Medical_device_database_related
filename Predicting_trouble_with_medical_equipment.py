ab27fc6d424b6bd167ae8efe4da4b1d7001fc039
Taichi Endoh <88502617+TaichiEndoh@users.noreply.github.com>
2021-12-11 23:37:04 +0900

    Update Predicting_trouble_with_medical_equipment.py

--- Predicting_trouble_with_medical_equipment.py
+++ Predicting_trouble_with_medical_equipment.py
@@ -12,6 +12,8 @@ import codecs
 from datetime import datetime
 from datetime import timedelta
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
 
 class my_directory_p:
     def __init__(self,pass_out):
@@ -23,7 +25,7 @@ class my_directory_p:
         data_dir = self.pass_out
         if not os.path.exists(data_dir):
             os.mkdir(data_dir)
-        print(u.pass_o()+"フォルダ作成しました！！")
+        print(u.pass_o()+"I created a folder!")
     def imp_data(self):
         image_file_path = './data/simulated_data/Medical_Device_simulated_data.csv'
         import pandas as pd
@@ -81,7 +83,7 @@ day_str_re=day_str_replace(df)
 
 #The data inspection date of the periodic inspection is set to day.
 periodic_inspection_p=periodic_inspection.rename(columns={'点検日': 'day'})
-#---------------------------------------------------------------------#
+
 #day as a number with day of the week data
 s_date=day_sum
 df_date=s_date.reset_index()
@@ -270,51 +272,33 @@ def Sort_by_date(allday):
     df_date=s_date.reset_index()
     df_date['day'] = pd.to_datetime(df_date['day'])
     df_date['曜日']=df_date['day'].apply(lambda x:x.weekday())
-    #日付順に並び替える
     sorted_df = df_date.sort_values(['day'])
     sorted_df2p=sorted_df.reset_index()
     sorted_df2=sorted_df2p.drop('index', axis=1)
-    sorted_df2
-    #datetime　からstr変換
     df_date=sorted_df2
     df_date['day'] = pd.to_datetime(df_date['day'])
     df_date['day'] = df_date['day'].astype(str)
-
     sorted_df3=df_date
-    #dayの('-', '/')を入れ替えたいので以下の方法を利用した
     df1 = sorted_df3
-
     sorted_df3r=df1['day'].str.replace('-', '/')
-    sorted_df3r
     df1_non_day=df1.drop('day', axis=1)
     df1_non_day['day']=sorted_df3r
     sorted_df3_d=df1_non_day
-    sorted_df3_d
     return sorted_df3_d
 
 sorted_df3_2nd=Sort_by_date(send2_allday)
-sorted_df3_2nd
-
 send2_ps_date=sorted_df3_2nd.fillna(0)
 send2_ps_date.to_csv(r""+u.pass_o()+'pre_data.csv', encoding = 'shift-jis')
-#dayのみ取り出しておく　series
 day_only=send2_ps_date["day"]
-
-#ワンホットエンコードのために　dayを消す
 send2_p1=send2_ps_date.drop(["day"], axis=1)# 削除
 send2_p=send2_p1.fillna(0)
-
-#ワンホットエンコードの実施
 df_one_hot_encoded = pd.get_dummies(send2_p)
 df_t=df_one_hot_encoded
-#ワンホットエンコードした後に　日付をくっつける
 day_o=pd.concat([day_only, df_t], axis=1)
 df_one_hot_encoded_p=day_o
 df=df_one_hot_encoded_p
 Fix_the_columns=df.loc[:,~df.columns.duplicated()]
 df_one_hot_encoded=Fix_the_columns
-df_one_hot_encoded
-
 apprix_df=df_one_hot_encoded
 apprix_1 = apprix_df.iloc[:200000,:]
 apprix_2 = apprix_df.iloc[200001:400000,:]
@@ -331,86 +315,47 @@ df=apprix_4
 df4=df.groupby('day').sum()
 df3_4=pd.concat([df3, df4])
 df1_4=pd.concat([df1_2, df3_4])
-df1_4
-df1_4_allday=df1_4
-df_d=df1_4_allday
-df_b1=df_d
+df_b1=df1_4
 df_b1['flg']=df_b1['target'].where(df_b1['target'] > 0, 0)
-
 df_b2=df_b1
 df_b2['flg']=df_b1['flg'].replace([1,2,3,4.0,5,6,7,8,9,10,27.0,12.0,16.0,24.0,180.0,160.0],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
 test_c=df_b2.drop(['target'], axis=1)
 df_one_hot_encoded=test_c.rename(columns={'flg':"target"})
-
 df_one_hot_encoded.to_csv(r""+u.pass_o()+'df_one_hot_encoded.csv', encoding = 'shift-jis')
-#データベースのデータ消えて使えなかったので諦め↑
-
 image_file_path2 = u.pass_o()+'df_one_hot_encoded_adjustment.csv'
 with codecs.open(image_file_path2, "r", "Shift-JIS", "ignore") as file:
                 df_one_hot_encoded_adjustment = pd.read_table(file, delimiter=",")
-
-#調整した値を入力
-df_one_hot_encoded_adjustment
-
 df_one_hot_encoded=df_one_hot_encoded_adjustment
-#pandasの中のcolumns（target）をn_day分ずらす変数　これによってn_day先を予測することができるfor文で回すことで調節が可能
-#variable that shifts columns (target) in pandas by n_days This allows us to predict n_days ahead, and can be adjusted by turning it with a for statement.
 def future_prediction_day(dfdata,culum_u,n_day):
-
         def Create_Description_X(dtt):
             train_data = dtt
-            X = train_data.drop(culum_u, axis=1)# 削除
-            #説明変数作成
-            #print("X.shape",X.shape)
+            X = train_data.drop(culum_u, axis=1)
             return X
-        #説明変数Create_Description_X実施これでXに値が入るはず
         Xp = Create_Description_X(dfdata)
-        #print("Create_Description_X 後の X.shape",X.shape)
-        #目的変数入力用関数
         def Objective_variable_creationY(Ymoto):
             targek = Ymoto
             Y=targek.T
             return Y
-        #目的変数作成
         targetk = dfdata[culum_u].values
-        #目的変数作成関数利用
         Yp = Objective_variable_creationY(targetk)
-        #pandsにもどす
         Y = pd.DataFrame({'target':Yp})
-        #pandasに戻す
         X = pd.DataFrame(Xp)
-        #目的変数の最初の行をn_day分削除
-        #print(Y)
         Y_future = Y[n_day:]
-        print(n_day,"日分削除した目的変数↓")
-        print("Y_future",Y_future)
         Y_future2 =Y_future.reset_index()
-        #説明変数の最後の行をn_day分削除
         X_future = X[:-n_day]
         X_future2 =X_future.reset_index()       
         Y_X_future2 =pd.concat([Y_future2, X_future2], axis=1)
         Y_X_future2
         Y_X_future2_o=Y_X_future2
         return Y_X_future2_o
-
 test1 = future_prediction_day(df_one_hot_encoded,"target",1)
-print(test1)
 test2 = future_prediction_day(df_one_hot_encoded,"target",2)
-print(test2)
 test3 = future_prediction_day(df_one_hot_encoded,"target",3)
-print(test3)
 test4 = future_prediction_day(df_one_hot_encoded,"target",4)
-print(test4)
 test5 = future_prediction_day(df_one_hot_encoded,"target",5)
-print(test5)
 test6 = future_prediction_day(df_one_hot_encoded,"target",6)
-print(test6)
 test7 = future_prediction_day(df_one_hot_encoded,"target",7)
-print(test7)
 test30 = future_prediction_day(df_one_hot_encoded,"target",30)
-print(test30)
-
-#この後にAI解析実施予定
 test1.drop("index", axis=1)
 test2.drop("index", axis=1)
 test3.drop("index", axis=1)
@@ -419,75 +364,40 @@ test5.drop("index", axis=1)
 test6.drop("index", axis=1) 
 test7.drop("index", axis=1) 
 test30.drop("index", axis=1) 
-
-tes=test7.drop("index", axis=1)# 削除
-
+tes=test7.drop("index", axis=1)
 def make_Base_line(df,numb):
-	import pandas as pd
-	import numpy as np
-	# Mean Absolute Error(MAE)用
-	from sklearn.metrics import mean_absolute_error
-	# Root Mean Squared Error(RMSE)
-	from sklearn.metrics import mean_squared_error
-	#以下のでカラムの値を取り出して名前を入れれる You can extract the value of the column and put the name in it as follows
 	columns_name = df.columns
-	#print(columns_name)
 	Serial_number= [x for x in range(len(columns_name))]
-	#print("Serial_number",Serial_number)
-	#print(columns_name[numb])
 	mean_1=df[columns_name[numb]].mean()
-	#print("columns_name and mean",mean_1)
 	lst = [df[columns_name[numb]].mean()] * len(df.index)     
-	## label data
 	label = df[str(columns_name[numb])]
-	## AI predicted data
 	pred = lst
-	# MAE計算
 	mae = mean_absolute_error(label, pred)
-	#print('MAE : {:.3f}'.format(mae))
-	# {:.3f}で小数点以下は3桁で表示
-	# RMSE計算
 	rmse = np.sqrt(mean_squared_error(label, pred))
-	#print('RMSE : {:.3f}'.format(rmse))
 	index1 = ["mean", 'MAE_mean', 'RMSE_mean']
 	columns1 =[str(columns_name[numb])]
 	Calculation=pd.DataFrame(data=[df[columns_name[numb]].mean(),format(mae),format(rmse)], index=index1, columns=columns1)
-	#print(Calculation)
 	return Calculation
 
 Calculation=make_Base_line(tes,0)
-Calculation
-
 test1.drop("index", axis=1)
 Calculation1=make_Base_line(test1,1)
-print("test1",Calculation1)
 test2.drop("index", axis=1)
 Calculation2=make_Base_line(test2,1)
-print("test2",Calculation2)
 test3.drop("index", axis=1)
 Calculation3=make_Base_line(test3,1)
-print("test3",Calculation3)
 test4.drop("index", axis=1) 
 Calculation4=make_Base_line(test4,1)
-print("test4",Calculation4)
 test5.drop("index", axis=1) 
 Calculation5=make_Base_line(test5,1)
-print("test5",Calculation5)
 test6.drop("index", axis=1) 
 Calculation6=make_Base_line(test6,1)
-print("test6",Calculation6)
 test7.drop("index", axis=1) 
 Calculation7=make_Base_line(test7,1)
-print("test7",Calculation7)
-
 tes=test30.drop("index", axis=1)
-
 tes1=test7.drop("index", axis=1)
-tes1
 test_c=tes1
-test_c
 merge_data=test_c
-
 # Isolate the objective variable
 X = merge_data.drop("target",axis=1).values
 y = merge_data["target"].values
@@ -537,19 +447,8 @@ y_test_df = pd.DataFrame(y_test)
 test_dfp = pd.concat([y_test_df,X_test_df], axis=1)
 test_df=test_dfp.rename(columns={0:"target"})
 marge_data_out=pd.concat([order_of_things, test_df])
-
-print("train shape", X_train.shape)
-print("test shape", X_test.shape)
-print("validation shape", X_val.shape)
 X_test_df = pd.DataFrame(X_test)
-print("y_train shape", y_train.shape)
-print("y_test shape", y_test.shape)
-print("y_validation shape", y_val.shape)
 y_test_df = pd.DataFrame(y_test)
-print("y_test describe",y_test_df.describe())
-print("not_ y_test describe",(~y_test_df.duplicated()).sum())
-print("y_test_df.duplicated().sum()",y_test_df.duplicated().sum())
-
 test_dfp = pd.concat([y_test_df,X_test_df], axis=1)
 test_df=test_dfp.rename(columns={0:"target"})
 test_dfp = pd.concat([y_test_df,X_test_df], axis=1)
@@ -577,7 +476,6 @@ X_valp = pd.DataFrame(X_res, columns=columns_name)
 val=pd.concat([y_valp, X_valp], axis=1)
 SMOTE_dfp=val.rename(columns={0:"target"})
 SMOTE_df=SMOTE_dfp.sample(frac=1, random_state=0)
-
 marge_data_over_sampling=pd.concat([SMOTE_df, test_df])
 test_classification=marge_data_over_sampling.drop("day", axis=1)
 
